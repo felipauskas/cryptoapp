9c4ed9c03beb4c08a4cc8a0144941fae237d4a3d
Felipe de Leon <felipe@felipedeleon.com>
2022-01-13 21:00:13 +0400

    added folder structure, along with the setup of Routes

---
+++ src/Pages/Coins/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Coins extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Coins Page</h1>
+            </div>
+        )
+    }
+}
