697c8c3e0e3d028506480fd6d18eca4d5bbc558b
Felipe de Leon <felipe@felipedeleon.com>
2022-02-07 19:34:57 +0400

    fixes done

--- src/components/PortfolioCoins/index.js
+++ src/components/PortfolioCoins/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCoin } from "store/coinDetails/detailsActions";
-
 import {
 	CoinDetails,
 	CoinDiv,
